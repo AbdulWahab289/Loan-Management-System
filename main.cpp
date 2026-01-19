db651e23f1ceffbb0ff6abe7e821fd4dcadfc584
LHRICSPM2266962 <lhricspm2266962@pgc.edu.pk>
2026-01-19 19:12:04 +0500

    Your message

--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+
+class User {
+public:
+    int id;
+    string name;
+    bool active;
+    
+    User* left;
+    User* right;
+
+    User(int id, string name) {
+        this->id = id;
+        this->name = name;
+        active = true;
+        left = NULL;
+        right = NULL;
+    }
+};
